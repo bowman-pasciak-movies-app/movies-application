8767b160ca7923c600a02937bb8af593c74598b1
Garrett Bowman <garrettbowman248@gmail.com>
2023-12-06 16:49:50 -0600

    Feat: Added Editting functionality to form

--- js/movies-api.js
+++ js/movies-api.js
@@ -28,3 +28,33 @@ export async function createMovie(movie) {
         console.error(error);
     }
 }
+
+export async function editMovie(id, movie) {
+    try {
+        const url = `http://localhost:3000/books/${id}`;
+        const options = {
+            method: "PATCH",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(movie)
+        };
+        const resp = await fetch(url, options);
+        return await resp.json();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export async function getMovieById(id) {
+    try {
+        // Get all the movies
+        const moviesUrl = `http://localhost:3000/movies/${id}`;
+        const moviesResponse = await fetch(moviesUrl);
+
+        // Return the movies array
+        return await moviesResponse.json();
+    } catch (error) {
+        console.error(error);
+    }
+}
